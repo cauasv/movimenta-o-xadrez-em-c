78148251f287eb1c8cd261d72b32d5cbc13f47ab
cauasv <caua.svicente2006@gmail.com>
2026-03-23 22:17:50 -0300

    Add files via upload

---
+++ xadrez.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// uso da recursividade na prática:
+void rainhamov1(int r) {
+    if (r > 0) {
+        printf("A rainha andou 1 casa/casas para a esquerda\n");
+        rainhamov1(r - 1);
+    }
+}
+
+int main() {
+
+    int r = 8;
+    int bispo = 1;
+    int torre;
+    int movimentocompleto1 = 1;
+
+    // movimentação da rainha
+    rainhamov1(r);
+
+    // movimentação do bispo
+    do {
+        printf("O Bispo se movimentou %d casa/casas na diagonal direita!\n", bispo);
+        bispo++;
+    } while (bispo <= 5);
+
+    // movimentação da torre
+    for (torre = 1; torre <= 5; torre++) {
+        printf("A torre se movimentou uma casa/casas para a direita\n");
+    }
+
+    // movimentação cavalo:
+    while (movimentocompleto1--) {
+        for (int cavalo = 0; cavalo < 2; cavalo++) {
+            printf("O cavalo foi para Cima\n");
+        }
+        printf("O cavalo foi para a Direita\n");
+        printf("O cavalo se movimentou uma vez!!!");
+    }
+
+    return 0;
+}
